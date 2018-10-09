487503a3c6a3eab6ae69d0e5f44c35987770f1e5
easily-distracted <rhollan2@our.ecu.edu.au>
2018-10-09 14:04:36 +0800

    Corrected for ydlidar

--- src/scan_to_pcl.cpp
+++ src/scan_to_pcl.cpp
@@ -26,7 +26,7 @@ ros::Publisher pcl_from_scan;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 laser_geometry::LaserProjection projector;
 
-void hokuyo_callbacks(const sensor_msgs::LaserScan::ConstPtr& scan_in)
+void ydlidar_callbacks(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
     sensor_msgs::PointCloud2 cloud;
     projector.projectLaser(*scan_in, cloud);
@@ -41,8 +41,8 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "laserScan_to_pointcloud");
     ros::NodeHandle nh;
 
-    ros::Subscriber hokuyo_sub;
-    hokuyo_sub = nh.subscribe<sensor_msgs::LaserScan>("/ydlidar_filtered", 1, hokuyo_callbacks);
+    ros::Subscriber ydlidar_sub;
+    ydlidar_sub = nh.subscribe<sensor_msgs::LaserScan>("/ydlidar_filtered", 1, ydlidar_callbacks);
 
 
     pcl_from_scan = nh.advertise<PointCloud>("ydlidar_points", 1);
